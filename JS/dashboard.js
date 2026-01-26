8e440eff6dcd70031c310e818068f6cd39a0c458
Cesar Zena <cymdocumentos@gmail.com>
2026-01-26 02:08:59 -0500

    HU-08 en progreso

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -3,12 +3,19 @@ if (localStorage.getItem("auth") !== "true") {
   window.location.href = "index.html";
 }
 
-// 🚪 Cerrar sesión (desde ambos botones)
+// 🚪 Cerrar sesión
 const logoutButtons = document.querySelectorAll("#logoutBtn, #logoutBtn2");
-
 logoutButtons.forEach(btn => {
   btn.addEventListener("click", () => {
     localStorage.removeItem("auth");
     window.location.href = "index.html";
   });
 });
+
+// 🚀 Navegación a Cuentas por Cobrar
+const btnCuentas = document.getElementById("btnCuentasPorCobrar");
+if (btnCuentas) {
+  btnCuentas.addEventListener("click", () => {
+    window.location.href = "deudas.html";
+  });
+}
